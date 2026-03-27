964136317b1856c0f2cc6d79a52d266b8735aa21
thanatat619 <thanatat.muan@bumail.net>
2026-03-27 12:41:12 +0700

    code

--- Assets/AngularVelocity.cs
+++ Assets/AngularVelocity.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 
-public class AngularVelocity
+public class AngularVelocity : MonoBehaviour
 {
-    
+    public float rotationSpeed = 5f; // rad/s
+
+    private Rigidbody rb;
+    private bool isStopped = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isStopped)
+        {
+            // หมุนตลอดเวลา
+            rb.angularVelocity = new Vector3(0f, rotationSpeed, 0f);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isStopped = !isStopped;
+        }
+    }
 }
